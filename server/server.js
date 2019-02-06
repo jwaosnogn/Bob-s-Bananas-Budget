8da6ab7e103aef6fe3398fb2ad29acb7cb195ed9
Jason Wong <jwaosnogn@gmail.com>
2019-02-05 23:53:33 -0800

    added proxy dev server, and created simple react ui

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Controll-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
